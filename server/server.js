13b96762ba70cfca56defdd3ab42c81ea49465b8
Jorge <jorgemedina2406@gmail.com>
2020-02-20 19:07:11 -0400

    Heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
