6e463637ca7986cf2c9e4913a7377dc6457d7f0d
Jorge <jorgemedina2406@gmail.com>
2020-02-20 20:10:08 -0400

    Cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-var moment = require('moment');
+const moment = require('moment');
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 const bodyParser = require('body-parser');
 
